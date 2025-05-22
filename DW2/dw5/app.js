39e92aeced0a684c09437446ce0d7a3377878b15
ELDZ-dev <eldzdevjr@gmail.com>
2025-05-22 18:37:36 -0300

    actualizacion clase 5 y 7

---
+++ DW2/dw5/app.js
@@ -0,0 +1,7 @@
+let nombreProducto = Fiambre
+let precioUnitario = 2600
+let cantidadDeseada = prompt(`¿Cuanto Fiambre queres?`)
+let precioFinal = cantidadDeseada*precioUnitario
+
+console.log(`compraste ${cantidadDeaseada}`)
+console.log (precioFinal)
---
+++ DW2/dw7/app7.js
@@ -0,0 +1,5 @@
+const colorTitulo = document.getElementById("nombreProducto")
+colorTitulo.style.color = "red"
+const cambioPrecio = document.getElementById("precioProducto")
+cambioPrecio.innerText = "$450"
+
